618cb64942e9f7728ccf8e20f3ba8ac577083470
aquaa-hub <jimhaudecoeur28@gmail.com>
2026-09-24 20:15:50 +0200

    Update dev_alert.py

--- dev_alert.py
+++ dev_alert.py
@@ -1,1 +1,10 @@
+import os
+import requests
 
+webhook = os.environ.get("DISCORD_WEBHOOK")
+
+if webhook:
+    requests.post(
+        webhook,
+        json={"content": "🟢 RL Dev Alert est connecté !"}
+    )
